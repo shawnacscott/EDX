bde7107438ae3dfe8302bb7696fd7e660fb88f98
Shawna C Scott <shawnatyger@gmail.com>
2013-09-09 21:09:21 -0700

    Adding more questions to the repo

--- Problem Set 2_Paying Off Credit Card Debt/Problem 1 Solution.py
+++ Problem Set 2_Paying Off Credit Card Debt/Problem 1 Solution.py
@@ -1,3 +1,10 @@
+#balance=the outstanding balance on the credit card
+#annualInterestRate=annual interest rate as a decimal
+#monthlyPaymentRate=minimum monthly payment rate as a decimal
+
+#balance=4842
+#annualInterestRate=0.2
+#monthlyPaymentRate=0.04
 totalPaid = 0
 for x in range(1, 13):
     print 'Month: ' + str(x)
@@ -11,3 +18,4 @@ for x in range(1, 13):
     print 'Remaining balance: ' + str(round(balance, 2))   
 print 'Total paid: ' + str(round(totalPaid, 2))
 print 'Remaining balance: ' + str(round(balance, 2))
+
